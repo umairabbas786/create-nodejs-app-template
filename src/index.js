66bd634aa92e9fe57ab2535a1819294d93482db5
Umair Abbas <umairabbass786@gmail.com>
2025-01-22 16:32:43 +0500

    🛠️ src/index.js -> Added CORS and body-parser middleware 🛠️ src/index.js -> Configured express to handle URLs 🛠️ src/index.js -> Introduced a new home route ("/")

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const loadRoutes = require("./loadRoutes");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 dotenv.config();
 connectDB();
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 loadRoutes(app);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 module.exports = app;
