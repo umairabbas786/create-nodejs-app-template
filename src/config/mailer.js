baa6994b6c56f062f939c006a2d0148f0e0650a1
Umair Abbas <umairabbass786@gmail.com>
2025-05-07 12:47:39 +0500

    🛠️ Update email configuration in .env.example and mailer.js

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -6,7 +6,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: process.env.MAIL_SERVICE || "gmail",
+  host: process.env.MAIL_HOST || "smtp.gmail.com",
+  port: process.env.MAIL_PORT || 587,
+  secure: process.env.MAIL_SECURE === "true",
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
